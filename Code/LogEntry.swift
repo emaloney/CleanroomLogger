cb895682b60ea76eb8396dd3c0d56edb24ce35fc
Evan Maloney <emaloney@gilt.com>
2015-05-06 00:30:45 -0400

    add a timestamp property, and an init() to set it

--- Code/LogEntry.swift
+++ Code/LogEntry.swift
@@ -45,4 +45,40 @@ public struct LogEntry
     /** A numeric identifier for the calling thread. Note that thread IDs are
     recycled over time. */
     public let callingThreadID: Int
+
+    /** The time at which the `LogEntry` was created. */
+    public let timestamp: NSDate
+
+    /**
+    `LogEntry` initializer.
+    
+    :param:     payload The payload of the `LogEntry` being constructed.
+    
+    :param:     severity The `LogSeverity` of the message being logged.
+    
+    :param:     callingFunction The signature of the function that issued the 
+                log request.
+    
+    :param:     callingFilePath The path of the source file containing the 
+                calling function that issued the log request.
+    
+    :param:     callingFileLine The line within the source file at which the log
+                request was issued.
+
+    :param:     callingThreadID A numeric identifier for the calling thread. 
+                Note that thread IDs are recycled over time.
+    
+    :param:     timestamp The time at which the log entry was created. Defaults
+                to the current time if not specified.
+    */
+    public init(payload: Payload, severity: LogSeverity, callingFunction: String, callingFilePath: String, callingFileLine: Int, callingThreadID: Int, timestamp: NSDate = NSDate())
+    {
+        self.payload = payload
+        self.severity = severity
+        self.callingFunction = callingFunction
+        self.callingFilePath = callingFilePath
+        self.callingFileLine = callingFileLine
+        self.callingThreadID = callingThreadID
+        self.timestamp = timestamp
+    }
 }
