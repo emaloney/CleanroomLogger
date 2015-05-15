9aff3a4732acfeef046f3715374deef7f7fd3a39
Evan Maloney <emaloney@gilt.com>
2015-05-15 10:44:38 -0400

    standardize documentation generation settings across Cleanroom projects

--- BuildControl/bin/generateDocumentation.sh
+++ BuildControl/bin/generateDocumentation.sh
@@ -9,8 +9,17 @@ fi
 pushd "`dirname $0`/../.." > /dev/null
 
 MODULE_NAME=`basename $PWD`
+CURRENT_YEAR=`date +"%Y"`
 
-"$JAZZY_EXECUTABLE" -o Documentation -m "$MODULE_NAME" --readme Code/README.md --github_url "https://github.com/emaloney/$MODULE_NAME" --author "Gilt Groupe" --author_url "http://tech.gilt.com/"
+"$JAZZY_EXECUTABLE" -o Documentation \
+	-m "$MODULE_NAME" \
+	--readme Code/README.md \
+	--github_url "https://github.com/emaloney/$MODULE_NAME" \
+	--author "Evan Maloney, Gilt Groupe" \
+	--author_url "http://github.com/emaloney" \
+	--copyright_holder "Gilt Groupe" \
+	--copyright_year "2014-$CURRENT_YEAR" \
+	--copyright_url "http://tech.gilt.com/"
 JAZZY_EXIT_CODE=$?
 if [[ $JAZZY_EXIT_CODE != 0 ]]; then
 	echo "error: $JAZZY_EXECUTABLE failed with an exit code of $JAZZY_EXIT_CODE; check any output above for additional details."
