0b5d80b7f43e0144d99ae6190f37fcfda89be383
Evan Maloney <emaloney@gilt.com>
2015-12-14 23:34:36 -0500

    add support for Swift Package Manager

---
+++ Package.swift
@@ -0,0 +1,8 @@
+import PackageDescription
+
+let package = Package(
+	name: "CleanroomLogger",
+	dependencies: [
+		.Package(url: "https://github.com/emaloney/CleanroomASL", versions: Version(1,3,7)..<Version(2,0,0))
+	]
+)
