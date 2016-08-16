da67c932f07ef664b6d188b6e41c71b0d2ddc86a
IamAlchemist <Wizard.Lee@qq.com>
2016-08-16 09:45:26 +0800

    delete checkout file

--- Carthage/Checkouts/CleanroomASL/Package.swift
+++
@@ -1,8 +0,0 @@
-import PackageDescription
-
-let package = Package(
-	name: "CleanroomASL",
-	dependencies: [
-		.Package(url: "https://github.com/emaloney/AppleSystemLogSwiftPackage", majorVersion: 1)
-	]
-)
