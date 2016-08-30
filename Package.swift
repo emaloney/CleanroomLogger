02221a5b0043d13179cd924e4cbba63a60edd808
Evan Maloney <emaloney@gilt.com>
2016-08-29 23:12:39 -0400

    this broke somewhere along the way; fix it

--- Package.swift
+++ Package.swift
@@ -1,5 +1,8 @@
 import PackageDescription
 
 let package = Package(
-    name: "CleanroomLogger"
+	name: "CleanroomLogger",
+	dependencies: [
+		.Package(url: "https://github.com/emaloney/CleanroomASL", versions: Version(1,4,2) ..< Version(2,0,0))
+	]
 )
