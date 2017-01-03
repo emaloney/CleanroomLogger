2ba02b4f6e916cb8d14238e4578d228c6835ff9c
Evan Maloney <emaloney@gilt.com>
2017-01-03 10:11:24 -0500

    remove CleanroomASL from package file

--- Package.swift
+++ Package.swift
@@ -2,7 +2,4 @@ import PackageDescription
 
 let package = Package(
 	name: "CleanroomLogger",
-	dependencies: [
-		.Package(url: "https://github.com/emaloney/CleanroomASL", versions: Version(1,5,0) ..< Version(2,0,0))
-	]
 )
